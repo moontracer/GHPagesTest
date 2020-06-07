3baa9453c0b424d58aa7d49674ea6c389b0f9d2d
moontracer <themoontracer@gmail.com>
2020-06-07 11:15:35 -0400

    Updates

---
+++ precache-manifest.f78147ca94597834cbc27fd0c7f03a46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e1a20801746628cd57a551e0ec07d8b",
+    "url": "/GHPagesTest/index.html"
+  },
+  {
+    "revision": "69d435d46c576358b50c",
+    "url": "/GHPagesTest/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4c0c1ca997ffcea7747b",
+    "url": "/GHPagesTest/static/js/2.c25e4985.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/GHPagesTest/static/js/2.c25e4985.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69d435d46c576358b50c",
+    "url": "/GHPagesTest/static/js/main.3ca8ef02.chunk.js"
+  },
+  {
+    "revision": "8fb86ddda403415a910f",
+    "url": "/GHPagesTest/static/js/runtime-main.248c5458.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/GHPagesTest/static/media/logo.5d5d9eef.svg"
+  }
+]);
